30bb8aed05bec331c5434545de2862d30e3b1310
Juliano <juliano.mendonca@aec.com.br>
2024-07-09 02:02:16 -0300

    feat: início da implementação dos testes

---
+++ TestingProject/TestContato.cs
@@ -0,0 +1,24 @@
+﻿using CadastroNumeros.Controllers;
+using CadastroNumeros.Implementations;
+using CadastroNumeros.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Linq;
+using Xunit;
+
+namespace TestingProject
+{
+    public class TestContato
+    {
+        [Fact]
+        public void Test_GetAll()
+        {
+            var contatoRepository = new Mock<IContatoRepository>();
+            var contatoController = new ContatoController(contatoRepository.Object);
+            var result = contatoController.GetAll();
+            var actionRsult = Assert.IsType<ActionResult>(result);
+            int count = result.Value.Count();
+            Assert.Equal(0, count);
+        }
+    }
+}
