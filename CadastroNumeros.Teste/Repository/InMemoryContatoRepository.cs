e5c6728f19bb71ded8610d9ecc9d06ff21140be5
Juliano <juliano.mendonca@aec.com.br>
2024-07-23 09:28:34 -0300

    feat: criação de testes em memória

---
+++ CadastroNumeros.Teste/Repository/InMemoryContatoRepository.cs
@@ -0,0 +1,128 @@
+﻿using CadastroNumeros.Domain.Models;
+using CadastroNumeros.Implementations;
+using CadastroNumeros.Infra.Data;
+using CadastroNumeros.Teste.Stubs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroNumeros.Teste.Repository
+{
+    public class InMemoryContatoRepository : IDisposable
+    {
+        private readonly AppDbContext context;
+
+        public InMemoryContatoRepository()
+        {
+            DbContextOptionsBuilder dbOptions = new DbContextOptionsBuilder()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var opts = new DbContextOptions<AppDbContext>(dbOptions.Options.Extensions.ToDictionary(e => e.GetType()));
+            context = new AppDbContext(opts);
+
+        }
+        public void Dispose() => context.Dispose();
+
+        public class DatabaseTests : IClassFixture<InMemoryContatoRepository>
+        {
+            ContatoRepository contatoRepository;
+
+            public DatabaseTests(InMemoryContatoRepository fixture)
+            {
+                contatoRepository = new ContatoRepository(fixture.context);
+            }
+
+            [Fact]
+            //CriarContato
+            public async Task TestMethod5()
+            {
+                //Arrange
+                var contato = new ContatoStub().Get();
+
+                //Act
+                var result = await contatoRepository.CriarContato(contato);
+
+                //Assert
+                var contatos = await contatoRepository.ListarContatos();
+                Assert.Single(contatos);
+                Assert.Equal(contatos.First(), result);
+            }
+
+
+            [Fact]
+            //ListarContatos
+            public async Task TestMethod4()
+            {
+                //Arrange
+                var stub = new ContatoStub();
+                var contatos = stub.Get(5);
+
+                foreach (var contato in contatos)
+                {
+                    await contatoRepository.CriarContato(contato);
+                }
+
+                //Act
+                var result = await contatoRepository.ListarContatos();
+
+                //Assert
+                Assert.NotEqual(result, contatos);
+                Assert.Equal(result.Count(), contatos.Count + 1);
+            }
+
+            [Fact]
+            //RetornarContato
+            public async Task TestMethod3()
+            {
+                // Arrange
+                var contatos = await contatoRepository.ListarContatos();
+                var contato = contatos.First();
+
+                // Act
+                var result = await contatoRepository.RetornarContato(contato.Id);
+
+                // Assert
+                Assert.Equal(contato, result);
+            }
+
+            [Fact]
+            //AtualizarContato
+            public async Task TestMethod2()
+            {
+                // Arrange
+                var contatos = await contatoRepository.ListarContatos();
+                var contatoAntigo = contatos.First();
+                var contatoNovo = contatoAntigo;
+                contatoNovo.Nome = "novo nome";
+
+                // Act
+                await contatoRepository.AtualizarContato(contatoNovo);
+                var result = await contatoRepository.RetornarContato(contatoNovo.Id);
+
+                // Assert
+                Assert.NotEqual(contatoAntigo.Nome, result.Nome);
+            }
+
+            [Fact]
+            //DeletarContato
+            public async Task TestMethod1()
+            {
+                // Arrange
+                var contatos = await contatoRepository.ListarContatos();
+                int quantidadeAnterior = contatos.Count();
+                var id = contatos.Last().Id;
+
+                // Act
+                await contatoRepository.DeletarContato(id);
+                var contatosAtuais = await contatoRepository.ListarContatos();
+                int quantidadeAtual = contatosAtuais.Count();
+
+                // Assert
+                Assert.Equal(quantidadeAnterior - 1, quantidadeAtual);
+            }
+        }
+    }
+}
---
+++ CadastroNumeros.Teste/Stubs/ContatoStub.cs
@@ -0,0 +1,59 @@
+﻿using CadastroNumeros.Domain.Models;
+
+namespace CadastroNumeros.Teste.Stubs
+{
+    public class ContatoStub
+    {
+        public Contato Get()
+        {
+            Contato contato = new()
+            {
+                DataCriacao = DateTime.Now,
+                Idade = 22,
+                Nome = "teste",
+                CodigoDdd = 84,
+                Email = "teste@email.com",
+                Id = Guid.NewGuid(),
+                Telefone = "123456789"
+            };
+
+            return contato;
+        }
+
+        public List<Contato> Get(int quantidade)
+        {
+            var lista = new List<Contato>();
+
+            for (var q = 0; q < quantidade; ++q)
+            {
+                lista.Add(
+                    new()
+                    {
+                        DataCriacao = DateTime.Now,
+                        Idade = int.Parse($"2{q}"),
+                        Nome = $"teste{q}",
+                        CodigoDdd = int.Parse($"8{q}"),
+                        Email = $"teste{q}@email.com",
+                        Id = Guid.NewGuid(),
+                        Telefone = $"12345678{q}"
+                    }
+                );
+            }
+
+            return lista;
+        }
+
+        //private readonly HashSet<string> DDDsValidos =
+        //[
+        //    "11", "12", "13", "14", "15", "16", "17", "18", "19",
+        //    "21", "22", "24", "27", "28", "31", "32", "33", "34", "35", "37", "38", "41", "42", "43", "44", "45", "46",
+        //    "47", "48", "49", "51", "53", "54", "55", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+        //    "71", "73", "74", "75", "77", "79", "81", "82", "83", "84", "85", "86", "87", "88", "89", "91", "92", "93", "94", "95", "96", "97", "98", "99"
+        //];
+
+        //public bool IsValid(string Codigo)
+        //{
+        //    return DDDsValidos.Contains(Codigo);
+        //}
+    }
+}
